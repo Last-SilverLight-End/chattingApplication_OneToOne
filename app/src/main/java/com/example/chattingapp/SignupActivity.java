295a4248947d3e2d7b6af1822c64925a060e99f2
Last-SilverLight-End <halfwing1998@gmail.com>
2021-08-02 22:50:14 +0900

    almost finished signing up

--- app/src/main/java/com/example/chattingapp/SignupActivity.java
+++ app/src/main/java/com/example/chattingapp/SignupActivity.java
@@ -5,12 +5,14 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Objects;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -32,6 +34,7 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import org.jetbrains.annotations.NotNull;
@@ -53,6 +56,25 @@ public class SignupActivity extends AppCompatActivity {
     private CheckBox CheckBox2;
     private static final int PICK_FROM_ALBUM = 10;
     private Uri imageUri;
+    private String imageUri_string="";
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,@Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_FROM_ALBUM ) {
+            if( resultCode == RESULT_OK) {
+                try {
+                    imageUri = data.getData(); //이미지 경로 원본
+                    imageUri_string = data.getDataString(); // 이미지 존재여부 확인
+                    Profile.setImageURI(data.getData()); // 가운데 뷰를 바꿈
+                } catch (Exception e) {
+                    Log.d(TAG, "Error about album preview: " + e.getMessage());
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,9 +113,9 @@ public class SignupActivity extends AppCompatActivity {
         Agree.setOnClickListener(new View.OnClickListener(){
 
 
+
             @Override
             public void onClick(View view){
-
 
                 String CheckBox1_string = CheckBox1.getText().toString();
                 String CheckBox2_string = CheckBox2.getText().toString();
@@ -101,7 +123,17 @@ public class SignupActivity extends AppCompatActivity {
                 String PasswordCheck_string = PasswordCheck.getText().toString();
                 String Id_string = Id.getText().toString().trim();
                 String Name_string = Name.getText().toString();
-                int Age_int = Integer.parseInt(Age.getText().toString());
+                String Age_string = Age.getText().toString();
+
+                String[] arr ={Password_string,Id_string,Name_string,Age_string};
+                for(String str : arr){
+                    //Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG).show();
+                    if(str.equals("")) {
+                        Toast.makeText(getApplicationContext(), "빈 내용이 존재합니다", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+
 
                 try {
                     FirebaseAuth.getInstance()
@@ -111,30 +143,45 @@ public class SignupActivity extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<AuthResult> task) {
 
                                         if (task.isSuccessful()) {
-
+                                            // 위의 입력하는 정보들 가져오는거 성공하면 파베에서 입력 하고
                                             UserModel userModel = new UserModel();
                                             userModel.userName = Name_string;
-                                            userModel.userAge = Age_int;
+                                            userModel.userAge = Age_string;
                                              String uid = task.getResult().getUser().getUid();
                                              FirebaseDatabase.getInstance().getReference().child("Users").child(uid).setValue(userModel);
-                                                FirebaseStorage.getInstance().getReference().child("UserImage").child(uid).putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                                                    @Override
-                                                    public void onComplete(@NotNull Task<UploadTask.TaskSnapshot> task) {
-                                                        if (task.isSuccessful()) {
-                                                            //@SuppressWarnings("VisibleForTests")
 
-                                                            String imgUri = task.getResult().getUploadSessionUri().toString();
-                                                            userModel.ProfileImageUri = imgUri;
-                                                            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                                                            startActivity(intent);
-                                                            finish();
-                                                        }
-                                                        else{
-                                                            Toast.makeText(getApplicationContext(), "사진이 존재하지 않습니다", Toast.LENGTH_LONG).show();
+                                             // 앨범에서 사진 가져오는데 성공 여부 판단 해서 결정하는곳
+
+
+                                                if(!(imageUri_string.equals(""))) {
+                                                    FirebaseStorage.getInstance().getReference().child("UserImage").child(uid).putFile(imageUri)
+                                                            .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                                                @Override
+                                                                public void onComplete(@NotNull Task<UploadTask.TaskSnapshot> task) {
+                                                                    if (task.isSuccessful()) {
+                                                                        //@SuppressWarnings("VisibleForTests")
+
+                                                                        String imgUri = task.getResult().getUploadSessionUri().toString();
+                                                                        userModel.ProfileImageUri = imgUri;
+                                                                        Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                                                        startActivity(intent);
+                                                                        finish();
+                                                                    } else {
+                                                                        Toast.makeText(getApplicationContext(), "사진이 존재하지 않습니다", Toast.LENGTH_LONG).show();
+
+                                                                    }
+                                                                }
+                                                            }).addOnFailureListener(new OnFailureListener() {
+                                                        @Override
+                                                        public void onFailure(@NonNull @NotNull Exception e) {
 
                                                         }
-                                                    }
-                                                });
+                                                    });
+                                                }
+                                                else{
+                                                    Toast.makeText(getApplicationContext(), "이미지가 없습니다", Toast.LENGTH_LONG).show();
+                                                    return;
+                                                }
                                             }
                                         else {
 
@@ -240,24 +287,6 @@ public class SignupActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_FROM_ALBUM ) {
-            if( resultCode == RESULT_OK) {
-                try {
 
-                    imageUri = data.getData(); //이미지 경로 원본
-                     Profile.setImageURI(data.getData()); // 가운데 뷰를 바꿈
-                } catch (Exception e) {
-                    Log.d(TAG, "Error about album preview: " + e.getMessage());
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-    private void moveActivity(){
-
-    }
 
 }
--- app/src/main/java/com/example/chattingapp/model/UserModel.java
+++ app/src/main/java/com/example/chattingapp/model/UserModel.java
@@ -3,5 +3,6 @@ package com.example.chattingapp.model;
 public class UserModel {
     public String userName;
     public String ProfileImageUri;
-    public int userAge;
+    public String ID;
+    public String userAge;
 }
